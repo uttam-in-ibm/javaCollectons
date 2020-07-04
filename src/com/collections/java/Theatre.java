c5ebbdaa0ecb000d2097b9b30d734416f951ac5e
node <node>
2020-07-04 12:29:02 +0530

    Linked List, Collection Interface and Binary Search

--- src/com/collections/java/Theatre.java
+++ src/com/collections/java/Theatre.java
@@ -4,10 +4,12 @@ import java.util.*;
 
 /**
  * Created by dev on 2/12/2015.
+ * https://docs.oracle.com/javase/tutorial/collections/interfaces/index.html
  */
 public class Theatre {
     private final String theatreName;
     private List<Seat> seats = new ArrayList<>();
+    //private Collection<Seat> seatsc = new LinkedList<>();
 
     public Theatre(String theatreName, int numRows, int seatsPerRow) {
         this.theatreName = theatreName;
@@ -25,21 +27,40 @@ public class Theatre {
         return theatreName;
     }
 
-    public boolean reserveSeat(String seatNumber) {
-        Seat requestedSeat = null;
-        for(Seat seat : seats) {
-            if(seat.getSeatNumber().equals(seatNumber)) {
-                requestedSeat = seat;
-                break;
-            }
-        }
+//    public boolean reserveSeat(String seatNumber) {
+//        int low = 0;
+//        int high = seats.size()-1;
+//
+//        while (low <= high) {
+//            System.out.print(".");
+//            int mid = (low + high) / 2;
+//            Seat midVal = seats.get(mid);
+//            int cmp = midVal.getSeatNumber().compareTo(seatNumber);
+//            System.out.println("Compare " + cmp);
+//
+//            if (cmp < 0) {
+//                low = mid + 1;
+//            } else if (cmp > 0) {
+//                high = mid - 1;
+//            } else {
+//                return seats.get(mid).reserve();
+//            }
+//        }
+//        System.out.println("There is no seat " + seatNumber);
+//        return false;
+//    }
 
-        if(requestedSeat == null) {
-            System.out.println("There is no seat " + seatNumber);
+    public boolean reserveSeat(String seatNumber) {
+        System.out.println("All Seats"+seats);
+        Seat requestedSeat = new Seat(seatNumber);
+        int foundSeat = Collections.binarySearch(seats,requestedSeat,null);
+        if(foundSeat>=0){
+            return seats.get(foundSeat).reserve();
+        }
+        else {
+            System.out.println("There is no seat "+seatNumber);
             return false;
         }
-
-        return requestedSeat.reserve();
     }
 
     // for testing
@@ -49,12 +70,18 @@ public class Theatre {
         }
     }
 
-    private class Seat {
+    private class Seat implements Comparable<Seat> {
         private final String seatNumber;
         private boolean reserved = false;
 
         public Seat(String seatNumber) {
             this.seatNumber = seatNumber;
+        }
+
+        // Need to implement this method for binary search to function
+        @Override
+        public int compareTo(Seat seat) {
+            return this.seatNumber.compareToIgnoreCase(seat.getSeatNumber());
         }
 
         public boolean reserve() {
@@ -81,23 +108,5 @@ public class Theatre {
             return seatNumber;
         }
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 }
