3fd719a66a28c3129c2afb5bf1d413fb52d41c76
node <node>
2020-07-04 12:54:50 +0530

    Collection List Methods

--- src/com/collections/java/Theatre.java
+++ src/com/collections/java/Theatre.java
@@ -4,12 +4,10 @@ import java.util.*;
 
 /**
  * Created by dev on 2/12/2015.
- * https://docs.oracle.com/javase/tutorial/collections/interfaces/index.html
  */
 public class Theatre {
     private final String theatreName;
-    private List<Seat> seats = new ArrayList<>();
-    //private Collection<Seat> seatsc = new LinkedList<>();
+    public List<Seat> seats = new ArrayList<>();
 
     public Theatre(String theatreName, int numRows, int seatsPerRow) {
         this.theatreName = theatreName;
@@ -27,40 +25,26 @@ public class Theatre {
         return theatreName;
     }
 
-//    public boolean reserveSeat(String seatNumber) {
-//        int low = 0;
-//        int high = seats.size()-1;
-//
-//        while (low <= high) {
-//            System.out.print(".");
-//            int mid = (low + high) / 2;
-//            Seat midVal = seats.get(mid);
-//            int cmp = midVal.getSeatNumber().compareTo(seatNumber);
-//            System.out.println("Compare " + cmp);
-//
-//            if (cmp < 0) {
-//                low = mid + 1;
-//            } else if (cmp > 0) {
-//                high = mid - 1;
-//            } else {
-//                return seats.get(mid).reserve();
-//            }
-//        }
-//        System.out.println("There is no seat " + seatNumber);
-//        return false;
-//    }
-
     public boolean reserveSeat(String seatNumber) {
-        System.out.println("All Seats"+seats);
-        Seat requestedSeat = new Seat(seatNumber);
-        int foundSeat = Collections.binarySearch(seats,requestedSeat,null);
-        if(foundSeat>=0){
-            return seats.get(foundSeat).reserve();
+        int low = 0;
+        int high = seats.size()-1;
+
+        while (low <= high) {
+            System.out.print(".");
+            int mid = (low + high) / 2;
+            Seat midVal = seats.get(mid);
+            int cmp = midVal.getSeatNumber().compareTo(seatNumber);
+
+            if (cmp < 0) {
+                low = mid + 1;
+            } else if (cmp > 0) {
+                high = mid - 1;
+            } else {
+                return seats.get(mid).reserve();
+            }
         }
-        else {
-            System.out.println("There is no seat "+seatNumber);
-            return false;
-        }
+        System.out.println("There is no seat " + seatNumber);
+        return false;
     }
 
     // for testing
@@ -70,7 +54,7 @@ public class Theatre {
         }
     }
 
-    private class Seat implements Comparable<Seat> {
+    public class Seat implements Comparable<Seat> {
         private final String seatNumber;
         private boolean reserved = false;
 
@@ -78,7 +62,6 @@ public class Theatre {
             this.seatNumber = seatNumber;
         }
 
-        // Need to implement this method for binary search to function
         @Override
         public int compareTo(Seat seat) {
             return this.seatNumber.compareToIgnoreCase(seat.getSeatNumber());
@@ -108,5 +91,23 @@ public class Theatre {
             return seatNumber;
         }
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
